d964254ff9cf672948f9fcaf612492feecc7e1c5
Pablo Zavala <juanparamosae@gmail.com>
2024-03-15 09:22:19 -0600

    ajustes de estilo

--- src/components/PaginationButton.jsx
+++ src/components/PaginationButton.jsx
@@ -2,7 +2,7 @@
 export const PaginationButton = ({ disableFlag, text, handleClick }) => {
     return (
         <>
-            <button className='rounded-xl bg-green-200 text-grey-600 px-2 min-w-36' disabled={!disableFlag} onClick={() => handleClick()}>{text}</button>
+            <button className='rounded-xl bg-green-400 text-grey-600 px-2 min-w-36 disabled:bg-green-100' disabled={!disableFlag} onClick={() => handleClick()}>{text}</button>
         </>
     )
 }
